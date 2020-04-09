900d21638b75a7415c3eab80798199d30086682b
Guoyu Ou <benogy@gmail.com>
2020-04-09 21:49:43 +0800

    Makefile changes

--- omake.lua
+++ omake.lua
@@ -1,7 +1,14 @@
 project = CreateProject()
 
-target = project:CreateLibrary("threadpool", STATIC | SHARED)
-target:AddSourceFiles("*.cpp")
-target:AddSysLibraries("pthread")
+dep = project:CreateDependency()
+dep:AddSourceFiles("*.cpp")
+dep:AddFlags("-Wall", "-Werror", "-Wextra", "-fPIC")
+dep:AddSysLibraries("pthread")
+
+a = project:CreateStaticLibrary("threadpool_static")
+a:AddDependencies(dep)
+
+so = project:CreateSharedLibrary("threadpool_shared")
+so:AddDependencies(dep)
 
 return project
--- tests/omake.lua
+++ tests/omake.lua
@@ -2,6 +2,7 @@ project = CreateProject()
 
 target = project:CreateBinary("test_threadpool")
 target:AddSourceFiles("*.cpp")
-target:AddLibrary("..", "threadpool", STATIC)
+target:AddFlags("-Wall", "-Werror", "-Wextra", "-fPIC")
+target:AddStaticLibrary("..", "threadpool_static")
 
 return project
